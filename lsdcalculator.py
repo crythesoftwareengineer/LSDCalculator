1da8941d83176eed59d5a454eb7a34c7cdb2bca4
crythesoftwareengineer <78820503+crythesoftwareengineer@users.noreply.github.com>
2021-02-09 13:26:10 -0600

    Add files via upload

---
+++ lsdcalculator.py
@@ -0,0 +1,10 @@
+print(""" 
+  _    ___ ___     ___      _         _      _           
+ | |  / __|   \   / __|__ _| |__ _  _| |__ _| |_ ___ _ _ 
+ | |__\__ \ |) | | (__/ _` | / _| || | / _` |  _/ _ \ '_|
+ |____|___/___/   \___\__,_|_\__|\_,_|_\__,_|\__\___/_|  
+            Made By cry#9999               """)
+acid = int(input("How Many LSD's Are You Taking?: "))
+
+math = acid * 100
+print("in result u will be taking the dosage of " + str(math) + "ug of acid")
